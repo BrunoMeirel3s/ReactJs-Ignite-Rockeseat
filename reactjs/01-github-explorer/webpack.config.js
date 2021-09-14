96e4181400d0f28801b29a69b34d1d96255fd66d
Bruno Meireles <brunomeireles23@gmail.com>
2021-09-13 21:37:24 -0300

    13-09-2021

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,3 +1,7 @@
+/**
+ * Usamos o webpack para ensinarmos o node a processar os arquivos de forma
+ * correta, como scss tsx e outros
+ */
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
@@ -7,11 +11,14 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 module.exports = {
   mode: isDevelopment ? "development" : "production",
   devtool: isDevelopment ? "eval-source-map" : "source-map",
+  //entry será o arquivo onde terá o código a ser convertido pelo webpack
   entry: path.resolve(__dirname, "src", "index.tsx"),
+  //output será o arquivos final gerado com o código já convertido
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  //extensão de arquivos que serão análisados pelo webpack
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
@@ -26,6 +33,7 @@ module.exports = {
     }),
   ].filter(Boolean),
   module: {
+    //Definimos as regras, qual o loader irá tratar determinados tipos de arquivos
     rules: [
       {
         test: /\.(j|t)sx$/,
